39c11a0386e74d433ff565ca44a17babcd4ea7b8
wangwei <w1220w@126.com>
2025-02-24 11:28:17 +0800

    feat: add tsconfig.json

--- dist/pomodoro-timer.d.ts
+++ dist/pomodoro-timer.d.ts
@@ -1,3 +1,11 @@
-export * from './index'
-import PomodoroTimer from './index'
-export default PomodoroTimer
+import { default as default_2 } from 'react';
+import { PomodoroSettings } from './types';
+
+declare function PomodoroTimer({ settings }: Props): default_2.JSX.Element;
+export default PomodoroTimer;
+
+declare interface Props {
+    settings: PomodoroSettings;
+}
+
+export { }
